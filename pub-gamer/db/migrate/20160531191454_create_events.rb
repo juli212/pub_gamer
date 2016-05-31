9d9e8eb76acd325afc598223c49bcaaf06ac7f35
juli212 <jjgiraldi@gmail.com>
2016-05-31 15:32:44 -0400

    create events migration

--- pub-gamer/db/migrate/20160531191454_create_events.rb
+++ pub-gamer/db/migrate/20160531191454_create_events.rb
@@ -1,6 +1,10 @@
 class CreateEvents < ActiveRecord::Migration
   def change
     create_table :events do |t|
+      t.string :title, null: false
+      t.text :description, null: false
+      t.datetime :event_date, null: false
+      t.integer :user_id, null: false
 
       t.timestamps null: false
     end
