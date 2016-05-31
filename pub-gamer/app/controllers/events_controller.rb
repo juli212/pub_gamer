8d6d97d44d4370a86d8123e7a4f2d3478376af68
juli212 <jjgiraldi@gmail.com>
2016-05-31 17:26:17 -0400

    add event routes and new event form

--- pub-gamer/app/controllers/events_controller.rb
+++ pub-gamer/app/controllers/events_controller.rb
@@ -2,4 +2,47 @@ class EventsController < ApplicationController
   def index
   	@events = Event.all
   end
+
+  def show
+  	@event = Event.find_by(id: params[:id])
+  end
+
+  def new
+	  @event = Event.new
+  end
+
+  def create
+  	binding.pry
+  	@event = Event.new(event_params)
+  	@event.user_id = current_user.id
+  	if @event.save
+	  redirect_to event_path(@event)
+  	else
+  	  @errors = @event.errors.full_messages
+      render 'new'
+  	end
+  end
+
+  def edit
+  	# if !logged_in?
+  	  redirect_to events_path
+  	# else
+  	  @user = User.find_by(id: session[:user_id])
+  	  @event = Event.find_by(id: params[:id])
+  		if @user.id == @event.user_id
+  		end
+  	# end
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  def event_params
+    params.require(:appointment).permit(:title, :date, :time, :description, :limit)
+  end
 end
