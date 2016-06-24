3a755ec631f2188591d228c5d25684cb24cac534
juli212 <jjgiraldi@gmail.com>
2016-06-24 16:02:56 -0400

    nest review route inside venue route, create review index and show partials

--- pub-gamer/app/controllers/reviews_controller.rb
+++ pub-gamer/app/controllers/reviews_controller.rb
@@ -1,12 +1,18 @@
 class ReviewsController < ApplicationController
 
-  def create
-    @review = Review.new()
-    rerender venue_path(@review.venue_id)
-  end
+	def create
+		@review = Review.new(review_params)
+		venue = Venue.find_by(id: params[:venue_id])
+		binding.pry
+		if request.xhr?
+			render partial: 'reviews/index', locals: { reviews: venue.reviews }
+		else
+			redirect_to venue_path(@venue)
+		end
+	end
 
-private
-  def review_params
-    params.require(:review).permit(:content, :vibe, :time_visited, :recommended)
-  end
+	private
+	def review_params
+		params.require(:review).permit(:content, :vibe, :time_visited, :recommended)
+	end
 end
