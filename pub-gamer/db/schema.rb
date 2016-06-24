c8a565a03cb70adc0402c372948620e559ce6a50
juli212 <jjgiraldi@gmail.com>
2016-06-24 12:50:19 -0400

    add some games to seed file

--- pub-gamer/db/schema.rb
+++ pub-gamer/db/schema.rb
@@ -84,11 +84,13 @@ ActiveRecord::Schema.define(version: 20160609034136) do
   end
 
   create_table "venues", force: :cascade do |t|
-    t.string   "name",       null: false
-    t.string   "address",    null: false
+    t.string   "name",        null: false
+    t.string   "address",     null: false
+    t.text     "description"
+    t.string   "place"
     t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
 end
