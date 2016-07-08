b2f1b706b02d11a57f64f2ea97abb238170b6b08
juli212 <jjgiraldi@gmail.com>
2016-07-08 17:29:26 -0400

    add presence validations to venue model

--- pub-gamer/app/models/venue.rb
+++ pub-gamer/app/models/venue.rb
@@ -1,10 +1,12 @@
 class Venue < ActiveRecord::Base
 	belongs_to :user
 	has_many :events
-  has_many :reviews
+  	has_many :reviews
 	has_many :venue_games
 	has_many :games, through: :venue_games
 	has_many :user_venues
+
+	validates :name, :address, presence: true
 
 	def game
 		
