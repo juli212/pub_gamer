d2dd72e3508d8153bf907d904a1ef1f8a9da8931
juli212 <jjgiraldi@gmail.com>
2016-07-08 17:17:46 -0400

    add presence validations to comment model

--- pub-gamer/app/models/comment.rb
+++ pub-gamer/app/models/comment.rb
@@ -1,6 +1,8 @@
 class Comment < ActiveRecord::Base
 	belongs_to :event
 
+	validates :body, :event_id, :user_id, presence: true
+	
 	def username
 		user = User.find_by(id: self.user_id)
 		user.user_name
