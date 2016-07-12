9b01b2c44340c96c2b44c47df8538d8ea874bf2a
juli212 <jjgiraldi@gmail.com>
2016-07-12 18:08:52 -0400

    remove commented out debuggers

--- pub-gamer/app/assets/javascripts/review_vibe.js
+++ pub-gamer/app/assets/javascripts/review_vibe.js
@@ -1,8 +1,6 @@
 $(document).ready(function() {
   $('.face img').on('click', function(event){
-    // target = event.target
     selected_div = event.target.parentNode;
-    // debugger;
     $('.selected').removeClass('selected')
     $(selected_div).addClass('selected')
     $(selected_div).find('input').prop('checked', true)
--- pub-gamer/app/assets/javascripts/reviews.js
+++ pub-gamer/app/assets/javascripts/reviews.js
@@ -2,13 +2,11 @@ $(document).ready(function() {
   $('#new_review').on('submit', function(event){
     event.preventDefault();
     $target = $(event.target)
-    // debugger;
     $.ajax({
       url: $target.attr('action'),
       method: $target.attr('method'),
       data: $target.serialize()
     }).done(function(response){
-      // debugger;
       $('#reviews').prepend(response);
       $('.selected').removeClass('selected')
       $('#new_review').each(function(){
