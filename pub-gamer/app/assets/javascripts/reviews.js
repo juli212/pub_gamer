51ee4601ca1181a207db5036640d4366beb3ce2d
juli212 <jjgiraldi@gmail.com>
2016-07-12 18:06:03 -0400

    clear vibe faces on successful save of new review

--- pub-gamer/app/assets/javascripts/reviews.js
+++ pub-gamer/app/assets/javascripts/reviews.js
@@ -2,14 +2,15 @@ $(document).ready(function() {
   $('#new_review').on('submit', function(event){
     event.preventDefault();
     $target = $(event.target)
-    debugger;
+    // debugger;
     $.ajax({
       url: $target.attr('action'),
       method: $target.attr('method'),
       data: $target.serialize()
     }).done(function(response){
-      debugger;
+      // debugger;
       $('#reviews').prepend(response);
+      $('.selected').removeClass('selected')
       $('#new_review').each(function(){
         this.reset();
       });
