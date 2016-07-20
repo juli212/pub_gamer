6662b03f27b45492e8b57086282131f453211348
juli212 <jjgiraldi@gmail.com>
2016-07-20 16:04:46 -0400

    add has_favorites? method to user model

--- pub-gamer/app/models/user.rb
+++ pub-gamer/app/models/user.rb
@@ -4,6 +4,8 @@ class User < ActiveRecord::Base
   has_many :reviews
   has_many :user_venues
   has_many :favorites, through: :user_venues, source: :venue
+  has_many :user_events
+  has_many :events, through: :user_events
 
   validates :user_name, :email, :password, presence: true
   validates :email, uniqueness: true
@@ -11,4 +13,9 @@ class User < ActiveRecord::Base
   def has_favorited?(venue)
   	self.favorites.include?(venue)
   end
+
+  def has_favorites?
+  	!self.favorites.empty?
+  end
+  
 end
