36924311aca8423e745f8eda88c58c31eaba2923
juli212 <jjgiraldi@gmail.com>
2016-07-25 14:01:34 -0400

    update controller logic to create review with or without vibes selected

--- pub-gamer/app/controllers/reviews_controller.rb
+++ pub-gamer/app/controllers/reviews_controller.rb
@@ -2,24 +2,19 @@ class ReviewsController < ApplicationController
 
 	def create
 		@review = Review.new(review_params)
-		# binding.pry
 		@review.venue_id = params[:venue_id]
 		venue = Venue.find_by(id: params[:venue_id])
 		@review.user_id = current_user.id
-		vibes = Vibe.find(params[:review][:vibes])
+		vibes = Vibe.find(params[:review][:vibes]) if params[:review][:vibes]
 		if @review.save && request.xhr?
-		binding.pry
-			@review.vibes = vibes
+			@review.vibes = vibes if vibes
 			render partial: 'reviews/show', locals: { review: @review }
 			@review = Review.new(review_params)
 			@current_rating = venue.avg_rating
 		elsif @review.save
-		binding.pry
-			binding.pry
-			@review.vibes = vibes
+			@review.vibes = vibes if vibes
 			redirect_to venue_path(venue)
 		else
-		binding.pry
 			@errors = @review.errors.full_messages
 			render partial: 'reviews/new'
 		end
