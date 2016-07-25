7a19fcb745e2ff6469e6ffb33595cdd672fd0b6c
juli212 <jjgiraldi@gmail.com>
2016-07-24 22:48:09 -0400

    update review model to have many vibes and fix venue model indentation

--- pub-gamer/app/models/review.rb
+++ pub-gamer/app/models/review.rb
@@ -1,6 +1,8 @@
 class Review < ActiveRecord::Base
   belongs_to :venue
   belongs_to :user
+  has_many :review_vibes
+  has_many :vibes, through: :review_vibes
 
   validates :content, :vibe, :venue_id, :user_id, presence: true
 
