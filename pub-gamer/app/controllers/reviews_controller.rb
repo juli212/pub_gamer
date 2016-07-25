a1e500fcd422e71fa48c2b3c49537a7a279c88a8
juli212 <jjgiraldi@gmail.com>
2016-07-25 03:29:38 -0400

    update review vibes to select multiple vibes

--- pub-gamer/app/controllers/reviews_controller.rb
+++ pub-gamer/app/controllers/reviews_controller.rb
@@ -1,17 +1,22 @@
 class ReviewsController < ApplicationController
 
 	def create
+		binding.pry
 		@review = Review.new(review_params)
+		binding.pry
 		@review.venue_id = params[:venue_id]
 		venue = Venue.find_by(id: params[:venue_id])
 		@review.user_id = current_user.id
 		if @review.save && request.xhr?
+		binding.pry
 			render partial: 'reviews/show', locals: { review: @review }
 			@review = Review.new(review_params)
 			@current_rating = venue.avg_rating
 		elsif @review.save
+		binding.pry
 			redirect_to venue_path(venue)
 		else
+		binding.pry
 			@errors = @review.errors.full_messages
 			render partial: 'reviews/new'
 		end
@@ -19,7 +24,7 @@ class ReviewsController < ApplicationController
 
 	private
 	def review_params
-		params.require(:review).permit(:content, :vibe, :rating, :venue_id)
+		params.require(:review).permit(:content, :rating, :venue_id, vibes: [])
 	end
 
 end
