717d778437d1cf8da70e6868fe53d2f6ac700ee8
juli212 <jjgiraldi@gmail.com>
2016-07-25 03:55:02 -0400

    update create review route to add multiple vibes

--- pub-gamer/app/controllers/reviews_controller.rb
+++ pub-gamer/app/controllers/reviews_controller.rb
@@ -1,19 +1,22 @@
 class ReviewsController < ApplicationController
 
 	def create
-		binding.pry
 		@review = Review.new(review_params)
-		binding.pry
+		# binding.pry
 		@review.venue_id = params[:venue_id]
 		venue = Venue.find_by(id: params[:venue_id])
 		@review.user_id = current_user.id
+		vibes = Vibe.find(params[:review][:vibes])
 		if @review.save && request.xhr?
 		binding.pry
+			@review.vibes = vibes
 			render partial: 'reviews/show', locals: { review: @review }
 			@review = Review.new(review_params)
 			@current_rating = venue.avg_rating
 		elsif @review.save
 		binding.pry
+			binding.pry
+			@review.vibes = vibes
 			redirect_to venue_path(venue)
 		else
 		binding.pry
@@ -24,7 +27,7 @@ class ReviewsController < ApplicationController
 
 	private
 	def review_params
-		params.require(:review).permit(:content, :rating, :venue_id, vibes: [])
+		params.require(:review).permit(:content, :rating, :venue_id)
 	end
 
 end
