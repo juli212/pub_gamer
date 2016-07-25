05511bead3912f8e01ecf8333d730b3a40b93b04
juli212 <jjgiraldi@gmail.com>
2016-07-25 15:37:39 -0400

    add basic styling for week selector and clean up js for review vibe and ratings

--- pub-gamer/app/assets/javascripts/review_vibe.js
+++ pub-gamer/app/assets/javascripts/review_vibe.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
   $('.face').on('click', function(event){
-  	if (event.target.nodeName == "DIV"){
-  		selected_div = event.target;
-  	} else if (event.target.nodeName == "IMG"){
-    	selected_div = event.target.parentElement;
-  	}
+  	// if (event.target.nodeName == "DIV"){
+  	// 	selected_div = event.target;
+  	// } else if (event.target.nodeName == "IMG"){
+   //  	selected_div = event.target.parentElement;
+  	// }
+  	selected_div = event.target.closest('.face')
    $(selected_div).toggleClass('selected');
    $(selected_div).find('input').prop('checked', !$(selected_div).find('input').prop('checked'));
   })
