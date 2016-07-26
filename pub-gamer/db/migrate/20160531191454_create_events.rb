f1398a41bec0203a107244396e508b1805f71142
Darrow87 <alexforgerii@gmail.com>
2016-07-26 14:30:07 -0400

    change event location from integer to string on migration

--- pub-gamer/db/migrate/20160531191454_create_events.rb
+++ pub-gamer/db/migrate/20160531191454_create_events.rb
@@ -5,8 +5,8 @@ class CreateEvents < ActiveRecord::Migration
       t.text :description, null: false
       t.date :event_date, null: false
       t.time :event_time, null: false
+      t.string :location, null: false
       t.integer :limit, null: false
-      t.integer :location, null: false
       t.integer :user_id, null: false
 
       t.timestamps null: false
