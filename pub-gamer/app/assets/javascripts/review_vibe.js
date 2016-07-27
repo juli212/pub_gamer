8a5b3ac57e315662319bb64824f5a989b91e2d28
juli212 <jjgiraldi@gmail.com>
2016-07-27 15:47:42 -0400

    delete commented out code

--- pub-gamer/app/assets/javascripts/review_vibe.js
+++ pub-gamer/app/assets/javascripts/review_vibe.js
@@ -1,19 +1,6 @@
 $(document).ready(function() {
   $('.face').on('click', function(event){
-<<<<<<< 3840b2dad8094564896057fa0c404bce8e59b2eb
-  	// if (event.target.nodeName == "DIV"){
-  	// 	selected_div = event.target;
-  	// } else if (event.target.nodeName == "IMG"){
-   //  	selected_div = event.target.parentElement;
-  	// }
   	selected_div = event.target.closest('.face')
-=======
-  	if (event.target.nodeName == "DIV"){
-  		selected_div = event.target;
-  	} else if (event.target.nodeName == "IMG"){
-    	selected_div = event.target.parentElement;
-  	}
->>>>>>> update review vibes to select multiple vibes
    $(selected_div).toggleClass('selected');
    $(selected_div).find('input').prop('checked', !$(selected_div).find('input').prop('checked'));
   })
