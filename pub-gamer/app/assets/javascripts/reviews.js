63a404fd2c6d60fc85bed9a8dcc357cc841e5fd7
juli212 <jjgiraldi@gmail.com>
2016-07-28 12:36:38 -0400

    add logic to append response to correct place when new review fails to create

--- pub-gamer/app/assets/javascripts/reviews.js
+++ pub-gamer/app/assets/javascripts/reviews.js
@@ -8,12 +8,19 @@ $(document).ready(function() {
       method: $target.attr('method'),
       data: $target.serialize()
     }).done(function(response){
-      $('.rated').removeClass('rated')
-      $('#reviews').prepend(response);
-      $('.selected').removeClass('selected')
-      $('#new_review').each(function(){
-        this.reset();
-      });
+      debugger;
+      if ($(response).hasClass('venue-show-left')) {
+        $('.rated').removeClass('rated')
+        $('#reviews').prepend(response);
+        $('.selected').removeClass('selected')
+        $('#new_review').each(function(){
+          this.reset();
+        });
+      } else {
+        $('#new-review-form').html(response);
+      }
+      // }debugger;
+    }).fail(function(response){
     })
     $.ajax({
       url: $target.children('#rating').attr('action'),
