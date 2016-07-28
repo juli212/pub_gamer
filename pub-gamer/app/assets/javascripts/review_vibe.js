35162716910eb32bafc7d1f1ba41545fd433f64f
juli212 <jjgiraldi@gmail.com>
2016-07-28 12:35:57 -0400

    modify js to show vibe text on hover

--- pub-gamer/app/assets/javascripts/review_vibe.js
+++ pub-gamer/app/assets/javascripts/review_vibe.js
@@ -1,7 +1,11 @@
 $(document).ready(function() {
   $('.face').on('click', function(event){
   	selected_div = event.target.closest('.face')
-   $(selected_div).toggleClass('selected');
-   $(selected_div).find('input').prop('checked', !$(selected_div).find('input').prop('checked'));
+  	$(selected_div).toggleClass('selected');
+  	$(selected_div).find('input').prop('checked', !$(selected_div).find('input').prop('checked'));
+  })
+  $('.face').hover(function(event){
+  	selected_div = event.target.closest('.face')
+  	$(selected_div).children('.vibe-name').toggleClass('show-vibe');
   })
 });
