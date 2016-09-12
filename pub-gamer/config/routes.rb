eedc6a6467d79bde732aa1753cd6518591e07e91
juli212 <jjgiraldi@gmail.com>
2016-09-12 11:46:08 -0400

    add review new route for jquery ui dialog to make modal

--- pub-gamer/config/routes.rb
+++ pub-gamer/config/routes.rb
@@ -24,7 +24,7 @@ Rails.application.routes.draw do
 
   resources :venues do
     get :search, :on => :collection
-    resources :reviews, only: [:create]
+    resources :reviews, only: [:new, :create]
   end
 
   # resources :venues do
