d683b474abea934408dbbb006a78bebcb9a1385c
juli212 <jjgiraldi@gmail.com>
2016-10-04 17:22:07 -0400

    add ui dropdown to venue create game field

--- pub-gamer/app/assets/javascripts/search.js
+++ pub-gamer/app/assets/javascripts/search.js
@@ -90,4 +90,51 @@ $(document).ready(function() {
 	  	$('.ui-state-focus').css('color', "#EDD0AF");
     }
 	})
+
+
+
+
+	$( "#other-text-field" ).autocomplete({
+		minLength: 1,
+		appendTo: "#add-venue-game-results",
+		source: function(request, response) {
+  	  $.ajax({
+        url: "/venues/add_games",
+        dataType: "json",
+        data: {
+            term: request.term
+        },
+        success: function(data) {
+	        response($.map(data, function(item) {
+	          // console.log(data)
+	          return {
+	            label: item.name,
+	            value: item.name,
+	            id: item.id
+	          };
+	        }))
+	    	}
+			})   
+	  },
+	  select: function(event, ui) {
+	  	$target = $(event.target)
+	  	// $('#event_address').val(ui.item.address);
+	  	// $('#event-venue_id').html("<input type='hidden' id='event_venue_id' name='event[venue_id]'>");
+	  	// $('#event_venue_id').val(ui.item.id);
+	  	// debugger;
+	  	// $.ajax({
+  	 //    url: 'events/update_games',
+    // 	  data: $('#event_venue_id').serialize()
+    // 	}).done(function(response){
+    // 		console.log(response);
+    // 		$('#event-create-games').html(response);
+    // 	})
+	  },
+	  focus: function(event, ui) {
+	  	$('.ui-menu-item').css('background-color', "#FFFFF0");
+	  	$('.ui-menu-item').css('color', "#2B2936");
+	  	$('.ui-state-focus').css('background-color', "#827AA4");
+	  	$('.ui-state-focus').css('color', "#EDD0AF");
+    }
+	})
 });
