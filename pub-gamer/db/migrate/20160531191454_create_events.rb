07ab2ba2d96e6f6720f1a5c0994d8968649ba4d8
juli212 <jjgiraldi@gmail.com>
2016-10-18 15:32:27 -0400

    delete awkward space

--- pub-gamer/db/migrate/20160531191454_create_events.rb
+++ pub-gamer/db/migrate/20160531191454_create_events.rb
@@ -5,7 +5,6 @@ class CreateEvents < ActiveRecord::Migration
       t.text :description, null: false
       t.date :date, null: false
       t.time :time, null: false
-      # t.string :location, null: false
       t.integer :limit, null: false
       t.boolean :deleted, null: false, default: false
       t.integer :user_id, null: false
