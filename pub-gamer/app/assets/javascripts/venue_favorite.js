6027b24ac6c5cc166a9fe4f1fde9782b4731f4c2
juli212 <jjgiraldi@gmail.com>
2017-01-25 16:46:52 -0500

    make favorite/remove favorite notice work on venue#show and venue#index

--- pub-gamer/app/assets/javascripts/venue_favorite.js
+++ pub-gamer/app/assets/javascripts/venue_favorite.js
@@ -1,14 +1,11 @@
 $(document).ready(function() {
   $('.user-favorite-status').on('click', 'a', function(event){
     event.preventDefault();
-    // debugger;
-    $('.index-bottom-notice p').text("");
+    $('.fav-notice').text("");
     $target = $(this)
     $.ajax({
       url: this.href,
       method: 'put'
-
     }).done()
-  // debugger;
   })
 });
--- pub-gamer/app/views/venues/dropdown.json.jbuilder
+++ pub-gamer/app/views/venues/dropdown.json.jbuilder
@@ -1,7 +1,5 @@
 json.array!(@results) do |result|
-
 	json.name					 result.name
 	json.address			 result.search_address
 	json.id						 result.id
-
 end
