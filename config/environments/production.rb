62b3f7a74f7d7aafbdd7cdce3887d3c3f3f64f02
juli212 <jjgiraldi@gmail.com>
2017-03-03 01:30:59 -0500

    change production mailer host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,6 +14,19 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { :host => 'pub-gamer.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "pubgamer.com",
+    user_name: ENV['username'],
+    password: ENV['password'],
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like
