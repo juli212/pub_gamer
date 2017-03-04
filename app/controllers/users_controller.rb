98deb9e738ee27e4312e43396dae4f4529e4eeb1
juli212 <jjgiraldi@gmail.com>
2017-03-03 20:35:57 -0500

    change show redirects

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -121,14 +121,11 @@ class UsersController < ApplicationController
         @created_events = @user.created_events
         @upcoming_events = @user.events
       end
-    else
-      redirect_to root_path
     end
   end
 
   def create
     @user = User.new(user_params)
-    
     if !request.xhr?
       if @user.save
         session[:user_id] = @user.id
