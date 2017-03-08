2e529ae9e3f3ff3f60f829d40b26f82ab3200d78
juli212 <jjgiraldi@gmail.com>
2017-03-08 14:51:13 -0500

    rename user_report to report

---
+++ app/controllers/report_controller.rb
@@ -0,0 +1,42 @@
+class ReportController < ApplicationController
+  before_filter :require_login
+
+	def new_inaccuracy
+		if request.xhr?
+			@report = Report.new
+			@report.venue_id = params[:format]
+			@report.topic = 2
+			render partial: 'new_inaccuracy'
+		end
+	end
+
+	def new_inquiry
+		if request.xhr?
+			@report = Report.new
+			@report.topic = 1
+			render partial: 'new_inquiry'
+		end
+	end
+
+	def create
+		@report = Report.new(report_params)
+		@report.user_id = current_user.id if current_user
+		binding.pry
+		if @report.save && @report.topic == "inquiry"
+			UserMailer.contact_us(@report).deliver_now
+			redirect_to contact_index_path, flash: { notice: "Message sent"}
+		elsif @report.save && @report.topic == "inaccuracy"
+			UserMailer.report_venue_inaccurate(@report).deliver_now
+			redirect_to venue_path(@report.venue), flash: { notice: "Message sent"}
+		else
+			redirect_to root_path, flash: { error: "Error sending message" }
+		end
+	end
+
+private
+	
+	def report_params
+		params.require(:report).permit(:topic, :content, :venue_id)
+	end
+
+end
--- app/models/user_report.rb
+++
@@ -1,8 +0,0 @@
-class UserReport < ActiveRecord::Base
-	belongs_to :user
-	belongs_to :venue
-
-	validates :user_id, :venue_id, :content, :topic, :status, presence: true
-	enum topic: [ :inquiry, :inaccuracy, :other ]
-	enum status: [ :unchecked, :progressing, :fixed, :unknown ]
-end
