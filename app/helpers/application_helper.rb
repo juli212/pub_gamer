3503c1e80f4a6df032b4277eabc3b8c2da6c0308
juli212 <jjgiraldi@gmail.com>
2017-03-15 17:32:34 -0400

    change notice to array

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
 	private
 
 	def require_login
-	  redirect_to root_path, flash: { notice: "You must be logged in to view page" } and return unless session[:user_id]
+	  redirect_to root_path, flash: { notice: ["You must be logged in to view page"] } and return unless session[:user_id]
   end
 
   def title(page_title)
