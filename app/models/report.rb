3bcb3af5cdac19973d7509e38bf5e122bd594d93
juli212 <jjgiraldi@gmail.com>
2017-10-25 16:02:39 -0400

    fix error rendering of contact form

--- app/models/report.rb
+++ app/models/report.rb
@@ -2,7 +2,7 @@ class Report < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :venue
 
-	validates :user_id, :content, :topic, :status, presence: true
+	validates :content, :topic, :status, presence: true
 	enum topic: [ :other, :inquiry, :inaccuracy]
 	enum status: [ :unknown, :unchecked, :progressing, :finished ]
 
