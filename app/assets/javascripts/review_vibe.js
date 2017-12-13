b8dc351c578b7e66aff6d1e5a3159b21d54c20e0
juli212 <jjgiraldi@gmail.com>
2017-12-13 18:10:20 -0500

    update rating selection indicators

--- app/assets/javascripts/review_vibe.js
+++ app/assets/javascripts/review_vibe.js
@@ -1,8 +1,12 @@
 $(document).ready(function() {
-  $('.face').on('click', function(event){
+  $('.face').on('touchstart click', function(event){
   	selected_div = event.target.closest('.face')
-  	$(selected_div).toggleClass('selected');
-  	$(selected_div).find('input').prop('checked', !$(selected_div).find('input').prop('checked'));
+  	if (event.type == "touchstart" || event.type == "click") {
+	  	$(selected_div).toggleClass('selected');
+	  	$(selected_div).find('input').prop('checked', !$(selected_div).find('input').prop('checked'));
+  		return false
+  	}
+  	return false
   })
   $('.face').hover(function(event){
   	selected_div = event.target.closest('.face')
--- app/assets/javascripts/time_of_week.js
+++ app/assets/javascripts/time_of_week.js
@@ -1,8 +1,12 @@
 $(document).ready(function() {
-	$('.week').on('click', function(event){
+	$('.week').on('touchstart click', function(event){
 		selected_div = event.target.closest('.week');
-		$('.time-of-week').not(selected_div).removeClass('time-of-week');
-		$(selected_div).toggleClass('time-of-week');
-  	$(selected_div).find('input').prop('checked', !$(selected_div).find('input').prop('checked'));
+		if (event.type == "touchstart" || event.type == "click") {
+			$('.time-of-week').not(selected_div).removeClass('time-of-week');
+			$(selected_div).toggleClass('time-of-week');
+  		$(selected_div).find('input').prop('checked', !$(selected_div).find('input').prop('checked'));
+			return false
+		}
+		return false
 	})
 });
