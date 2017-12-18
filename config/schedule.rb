ae4ef47ebda4107b5b9b871e3081fd08df7f87a5
juli212 <jjgiraldi@gmail.com>
2017-12-18 18:27:04 -0500

    add daily email test rake task

--- config/schedule.rb
+++ config/schedule.rb
@@ -19,6 +19,11 @@
 
 # Learn more: http://github.com/javan/whenever
 
+<<<<<<< 9b3603c7b70a4e6454cd1c33abe00d0f94950d78
 every :day, at: '6:10 pm' do
 	runner UserMailer.test_email().deliver_now
+=======
+every :day, at: '6:30 pm' do
+	rake "send_test_email"
+>>>>>>> add daily email test rake task
 end
